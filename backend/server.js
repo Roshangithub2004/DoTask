bd412d118412f9f03240e54ab60bbd5857e5d207
Roshan Shirbhate <roshanshirbhate654@gmail.com>
2025-12-26 11:59:34 +0530

    full stack todo app

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,14 @@ app.use("/api/todos/", todoRoute)
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname, "frontend/dist")))
-    app.get("/", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
 }
+
 
 
 connectDB()
