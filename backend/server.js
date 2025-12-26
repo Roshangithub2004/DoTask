30402f8c7d6ea62ab06f733aa287d3c5b92d2dcd
Roshan Shirbhate <roshanshirbhate654@gmail.com>
2025-12-26 17:10:01 +0530

    remove serving code

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv"
 import {connectDB} from "./config/db.js"
 import todoRoute from "./routes/todo.route.js";
-import path from "path"
 const PORT = process.env.PORT || 3000
 
 dotenv.config()
@@ -15,16 +14,6 @@ app.use(express.json())
 app.use("/api/todos/", todoRoute)
 
 const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
-
 
 connectDB()
 .then(()=>{
